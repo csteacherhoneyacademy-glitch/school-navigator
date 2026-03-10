c8e60ab410dcd1c412f3e642eadcead6ea86e7cb
csteacherhoneyacademy-glitch <csteacher.honeyacademy@gmail.com>
2026-03-10 08:48:12 +0200

    Update floor4.py

--- floor4.py
+++ floor4.py
@@ -3,38 +3,47 @@ floor_data = {
         "name": "Sport",
         "teacher": "Пан Віталій, Пані Ірина",
         "description": "Кімната знепритомнення",
-        "photo": "images/101.jpg"
+        "photo": "images/42.jpg"
     },
     "41": {
-        "name": "Individual",
+        "name": "Accounting department",
         "teacher": "n/a",
         "description": "незвідана зона відчуження",
-        "photo": "images/102.jpg"
+        "photo": "images/41.jpg"
     },
  "43": {
         "name": "Foreign language",
         "teacher": "Elijah Obiefuleh",
-        "description": "Реанімаційна",
-        "photo": "images/103.jpg"
+        "description": "Реанімаційна”,
+        "photo": "images/43.jpg"
     },
 "44": {
         "name": "Foreign language",
         "teacher": "n/a",
         "description": "незвідана зона вітчуження",
-        "photo": "images/104.jpg"
+        "photo": "images/44.jpg"
     },
 "45": {
         "name": "Foreign language",
         "teacher": "Пан Олексій",
         "description": "незвідана зона вітчуження",
-        "photo": "images/103.jpg"
+        "photo": "images/45.jpg"
     },
 "46": {
-        "name": "n/a",
+        "name": "Multiperpose room",
         "teacher": "n/a",
         "description": "незвідана зона вітчуження",
-        "photo": "images/103.jpg"
+        "photo": "images/46.jpg"
     }
-
-}
+"47": {
+        "name": "HR department",
+        "teacher": "n/a",
+        "description": "незвідана зона вітчуження",
+        "photo": "images/47.jpg"
+    }
+"WC4": {
+        "name": "WC",
+        "teacher": "n/a",
+        "description": "місце збору",
+        "photo": "images/WC4.jpg"
 

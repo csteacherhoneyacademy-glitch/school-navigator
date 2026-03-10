d45c89d4d678c5684c2fe77a131d1068168ab4e6
csteacherhoneyacademy-glitch <csteacher.honeyacademy@gmail.com>
2026-03-10 09:34:54 +0200

    Update floor3.py

--- floor3.py
+++ floor3.py
@@ -1,6 +1,6 @@
 floor_data = {
- "31": 
-        "name": “Foreign language”
+ "31": {
+        "name": "Foreign language",
         "teacher": "Хто завгодно",
         "description": "тут ми вчимо англійську",
         "photo": "images/31.jpg"
@@ -8,38 +8,38 @@ floor_data = {
           },
 
     "32": {
-        "name", “кабінет інформатики”
+        "name":"кабінет інформатики",
         "teacher": "вчителі інформатики",
         "description": "каінет 11- випускників 15",
         "photo": "images/32.jpg"
 
           },
     "33": {
-      “name”:”кабінет математики”
+      "name":"кабінет математики",
         "teacher": "пані Анна",
         "description": "ворота в пекло",
         "photo": "images/33.jpg"
-    }
- "34": { 
-     ,
+    },
+ "35": { 
+     
          "name": "кабінет математики",
         "teacher": "Пані Марія",  
          "description": "каторжна зона",
-        "photo": "images/34.jpg"
-    }
- "35": {
+        "photo": "images/35.jpg"
+    },
+ "36": {
         "name": "кабінет історії",
         "teacher": "пані Ольга",
         "description": "кабінет Середньовіччя викладач та антураж",
         "photo": "images/35.jpg"
-    }
- "36": {
+    },
+ "37": {
     
         "name": "кабінет англ мови",
         "teacher": "n\a",
         "description": "вінегрет 9,10,11",
         "photo": "images/36.jpg"
-    }
+    },
  "WC 3": {
       
         "name": "туалет чоловічий",
